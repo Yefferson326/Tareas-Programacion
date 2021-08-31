46630925e5d3132e81f37c0e98cda428f45af359
Yefferson326 <yefferson.nunez@uptc.edu.co>
2021-08-31 17:27:44 -0500

    Se le agrego al programa las condiciones de que las notas introducidas no podian ser menores a 0 ni mayores a 5.0 y se le anexo al resultado final la opcion de imprimir en pantalla si el estudiante aprobaba o reprobaba, esto teniendo en cuenta de que se aprobaba con 2.95

--- Promedios/Notas.c
+++ Promedios/Notas.c
@@ -2,31 +2,57 @@
 
 int main (){
 
-    float note_one;
-    float note_two;
-    float note_three;
-    float note_four;
-    float note_five;
+    float noteOne;
+    float noteTwo;
+    float noteThree;
+    float noteFour;
+    float noteFive;
     float promedio;
 
-    printf("Bienevenido \nEn este programa podra organizar las notas de sus estudiantes y conocer su promedio\n");
+    printf("Bienvenido \nEn este programa podra organizar las notas de sus estudiantes y conocer su promedio\n");
     
 
-    printf("ingrese las notas del estudiante\n");
+    printf("Ingrese las notas del estudiante\n");
     printf("1.");
-    scanf("%f", & note_one);
-    printf("2.");
-    scanf("%f", & note_two);
-    printf("3.");
-    scanf("%f", & note_three);
-    printf("4.");
-    scanf("%f", & note_four);
-    printf("5.");
-    scanf("%f", & note_five);
+    scanf("%f", & noteOne);
+    if(noteOne >= 0 && noteOne <= 5.0){
+        printf("2.");
+        scanf("%f", & noteTwo);
+        if(noteTwo >= 0 && noteTwo <= 5.0){
+            printf("3.");
+            scanf("%f", & noteThree);
+            if(noteThree >= 0 && noteThree <= 5.0){
+                printf("4.");
+                scanf("%f", & noteFour);
+                if(noteFour >= 0 && noteFour <= 5.0){
+                    printf("5.");
+                    scanf("%f", & noteFive);
+                    if(noteFive >= 0 && noteFive <= 5.0){
+                        promedio = (noteOne + noteTwo + noteThree + noteFour + noteFive)/5;
+                         if (promedio >= 2.95){
+                             printf("El estudiante aprobo la asignatura con un promedio de: %f",promedio);
+                         }else{
+                             printf("El estudiante reprobo la asignatura con un promedio de: %f",promedio);
+                         }
 
-    promedio = (note_one + note_two + note_three + note_four + note_five)/5;
+                    }else{
+                        printf("Por favor ingrese la nota 5 en el rango de 0 hasta 5.0");
+                    }
 
-    printf("El promedio de las notas es: %f",promedio);
+                }else{
+                    printf("Por favor ingrese la nota 4 en el rango de 0 hasta 5.0");
+                }
+
+            }else{
+                printf("Por favor ingrese la nota 3 en el rango de 0 hasta 5.0");
+            }
+
+        }else{
+            printf("Por favor ingrese la nota 2 en el rango de 0 hasta 5.0");
+        }
+    }else{
+        printf("Por favor ingrese la nota 1 en el rango de 0 hasta 5.0");
+    }
 
     return 0;
 }
