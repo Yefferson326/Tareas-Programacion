3d67d7208ae7cded463004e38ae6d3ea26395512
Yefferson326 <yefferson.nunez@uptc.edu.co>
2021-09-08 17:12:30 -0500

    agregue los dos productos mas que hacian falta para un total de 8

--- Proyecto/Proyecto.c
+++ Proyecto/Proyecto.c
@@ -41,12 +41,12 @@ int dateAndTime (){
 
 
 int main (){
-    int numberOfoneProduct,numberOfTwoProduct,numberOfthreeProduct,numberOffourProduct,numberOffiveProduct,numberOfsixProduct;
-    float oneProductValue,totalValueOneProduct,twoProductValue,totalValueTwoProduct,threeProductValue,totalValuethreeProduct,fourProductValue,totalValuefourProduct,fiveProductValue,totalValuefiveProduct,sixProductValue,totalValuesixProduct;
+    int numberOfoneProduct,numberOfTwoProduct,numberOfthreeProduct,numberOffourProduct,numberOffiveProduct,numberOfsixProduct,numberOfSevenProduct,numberOfEigthProduct;
+    float oneProductValue,totalValueOneProduct,twoProductValue,totalValueTwoProduct,threeProductValue,totalValuethreeProduct,fourProductValue,totalValuefourProduct,fiveProductValue,totalValuefiveProduct,sixProductValue,totalValuesixProduct,sevenProductValue,totalValueSevenProduct,eigthProductValue,totalValueEigthProduct;
     float totalpurchase,totalDiscount;
-    char oneProductName[20],twoProductName[20],threeProductName[20],fourProductName[20],fiveProductName[20],sixProductName[20];
+    char oneProductName[20],twoProductName[20],threeProductName[20],fourProductName[20],fiveProductName[20],sixProductName[20],sevenProductName[20],eigthProductName[20];
     char newProduct,validTv;
-    printf("**********BIENVENIDO**********\nEn este programa podras conocer el valor total de tus productos y mirar tu descuento para el mes de Septiembre\n");
+    printf("**********BIENVENIDO**********\nEn este programa podras conocer el valor total de 8 productos y mirar tu descuento para el mes de Septiembre\n");
     printf("FACTURA\n");
     dateAndTime();
     system("pause");
@@ -110,38 +110,70 @@ int main (){
                             printf("Cantidad:");
                             scanf("%i",&numberOfsixProduct);
                             totalValuesixProduct = sixProductValue * numberOfsixProduct;
-                            printf("<<<Limite de productos facturados 6>>>");
-                            printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,sixProductName,37);
-                            totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct+totalValuefourProduct+totalValuefiveProduct+totalValuesixProduct;
-                            totalDiscount = totalpurchase-((totalpurchase)*0.10);
-                            printf("Total compra:$%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalpurchase,37,totalDiscount);
+                            printf("Desea ingresar un producto mas a la factura\nSI[Presione 's']\nNO[Presione 'n']\n--> ");
+                            scanf("%s",&newProduct);
+                            if(newProduct == 's'){
+                                printf("Escriba el nombre del producto: ");
+                                scanf("%s",&sevenProductName);
+                                printf("Valor del producto (%s): $",sevenProductName);
+                                scanf("%f",&sevenProductValue);
+                                printf("Cantidad:");
+                                scanf("%i",&numberOfSevenProduct);
+                                totalValueSevenProduct = sevenProductValue * numberOfSevenProduct;
+                                printf("Desea ingresar un producto mas a la factura\nSI[Presione 's']\nNO[Presione 'n']\n--> ");
+                                scanf("%s",&newProduct);
+                                if(newProduct == 's'){
+                                    printf("Escriba el nombre del producto: ");
+                                    scanf("%s",&eigthProductName);
+                                    printf("Valor del producto (%s): $",eigthProductName);
+                                    scanf("%f",&eigthProductValue);
+                                    printf("Cantidad:");
+                                    scanf("%i",&numberOfEigthProduct);
+                                    totalValueEigthProduct = eigthProductValue * numberOfEigthProduct;
+                                    printf ( " <<< Limite de productos facturados 8 >>> " );
+                                    printf ( "PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n DESCUENTO: 10 % c al valor total de la compra " , oneProductName, twoProductName, threeProductName, fourProductName, fiveProductName, sixProductName,sevenProductName,eigthProductName, 37 );
+                                    totalpurchase = totalValueOneProduct + totalValueTwoProduct + totalValuethreeProduct + totalValuefourProduct + totalValuefiveProduct + totalValuesixProduct + totalValueSevenProduct + totalValueEigthProduct;
+                                    totalDiscount = totalpurchase - ((totalpurchase) * 0.10 );
+                                    printf ( "Total compra: $%f\nValor total a pagar (con el 10 % c de descuento): $%f" , totalpurchase, 37 , totalDiscount);
+                                }else{
+                                    printf("PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,sixProductName,sevenProductName,37);
+                                    totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct+totalValuefourProduct+totalValuefiveProduct+totalValuesixProduct+totalValueSevenProduct;
+                                    totalDiscount = totalpurchase-((totalpurchase)*0.10);
+                                    printf("Total compra: $%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalpurchase,37,totalDiscount);
+                                }
+                            }else{
+                                printf("PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,sixProductName,37);
+                                totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct+totalValuefourProduct+totalValuefiveProduct+totalValuesixProduct;
+                                totalDiscount = totalpurchase-((totalpurchase)*0.10);
+                                printf("Total compra: $%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalpurchase,37,totalDiscount);
+                            }
                         }else{
-                            printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,37);
+                            printf("PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,37);
                             totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct+totalValuefourProduct+totalValuefiveProduct;
                             totalDiscount = totalpurchase-((totalpurchase)*0.10);
                             printf("Total compra: $%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalpurchase,37,totalDiscount);
                         }
                     }else{
-                        printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,37);
+                        printf("PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,37);
                         totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct+totalValuefourProduct;
                         totalDiscount = totalpurchase-((totalpurchase)*0.10);
                         printf("Total compra: $%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalpurchase,37,totalDiscount);
                     }
                 }else{
-                    printf("PRODUCTOS:\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,37);
+                    printf("PRODUCTOS:\n-> %s\n-> %s\n-> %s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,37);
                     totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct;
                     totalDiscount = totalpurchase-((totalpurchase)*0.10);
                     printf("Total compra: $%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalpurchase,37,totalDiscount);
                 }
              }else{
-                printf("PRODUCTOS:\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,37);
+                printf("PRODUCTOS:\n-> %s\n-> %s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,37);
                 totalpurchase=totalValueOneProduct+totalValueTwoProduct;
                 totalDiscount = totalpurchase-((totalpurchase)*0.10);
                 printf("Total compra: $%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalpurchase,37,totalDiscount);
              }
         
         }else{
-            printf("PRODUCTOS:\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,37);
+            printf("PRODUCTOS:\n-> %s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,37);
             totalDiscount = totalValueOneProduct-(totalValueOneProduct*0.10);
             printf("Total compra: $%f\nTotal valor a pagar (con el 10%c de descuento): $%f",totalValueOneProduct,37,totalDiscount);
         }
@@ -204,38 +236,69 @@ int main (){
                             printf("Cantidad:");
                             scanf("%i",&numberOfsixProduct);
                             totalValuesixProduct = sixProductValue * numberOfsixProduct;
-                            printf("<<<Limite de productos facturados 6>>>");
-                            printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,sixProductName,37);
-                            totalpurchase= totalValueOneProduct + totalValueTwoProduct + totalValuethreeProduct + totalValuefourProduct + totalValuefiveProduct + totalValuesixProduct;
-                            totalDiscount = totalpurchase-((totalpurchase)*0.15);
-                            printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalpurchase,37,totalDiscount);
-                            
+                            printf("Desea ingresar un producto mas a la factura\nSI[Presione 's']\nNO[Presione 'n']\n--> ");
+                            scanf("%s",&newProduct);
+                            if(newProduct == 's'){
+                                printf("Escriba el nombre del producto: ");
+                                scanf("%s",&sevenProductName);
+                                printf("Valor del producto (%s): $",sevenProductName);
+                                scanf("%f",&sevenProductValue);
+                                printf("Cantidad:");
+                                scanf("%i",&numberOfSevenProduct);
+                                totalValueSevenProduct = sevenProductValue * numberOfSevenProduct;
+                                printf("Desea ingresar un producto mas a la factura\nSI[Presione 's']\nNO[Presione 'n']\n--> ");
+                                scanf("%s",&newProduct);
+                                if(newProduct == 's'){
+                                    printf("Escriba el nombre del producto: ");
+                                    scanf("%s",&eigthProductName);
+                                    printf("Valor del producto (%s): $",eigthProductName);
+                                    scanf("%f",&eigthProductValue);
+                                    printf("Cantidad:");
+                                    scanf("%i",&numberOfEigthProduct);
+                                    totalValueEigthProduct = eigthProductValue * numberOfEigthProduct;
+                                    printf ( " <<< Limite de productos facturados 8 >>> " );
+                                    printf ( "PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n DESCUENTO: 15%c al valor total de la compra " , oneProductName, twoProductName, threeProductName, fourProductName, fiveProductName, sixProductName,sevenProductName,eigthProductName, 37 );
+                                    totalpurchase = totalValueOneProduct + totalValueTwoProduct + totalValuethreeProduct + totalValuefourProduct + totalValuefiveProduct + totalValuesixProduct + totalValueSevenProduct + totalValueEigthProduct;
+                                    totalDiscount = totalpurchase - ((totalpurchase) * 0.15 );
+                                    printf ( "Total compra: $%f\nValor total a pagar (con el 15%c de descuento): $%f" , totalpurchase, 37 , totalDiscount);
+                                }else{
+                                    printf("PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\nDESCUENTO: 15%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,sixProductName,sevenProductName,37);
+                                    totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct+totalValuefourProduct+totalValuefiveProduct+totalValuesixProduct+totalValueSevenProduct;
+                                    totalDiscount = totalpurchase-((totalpurchase)*0.15);
+                                    printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalpurchase,37,totalDiscount);
+                                }
+                            }else{
+                                printf("PRODUCTOS:\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\n-> %s\nDESCUENTO: 15%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,sixProductName,37);
+                                totalpurchase=totalValueOneProduct+totalValueTwoProduct+totalValuethreeProduct+totalValuefourProduct+totalValuefiveProduct+totalValuesixProduct;
+                                totalDiscount = totalpurchase-((totalpurchase)*0.15);
+                                printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalpurchase,37,totalDiscount);
+                            }
                         }else{
-                            printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,37);
+                            printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 15%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,fiveProductName,37);
                             totalpurchase =  totalValueOneProduct + totalValueTwoProduct + totalValuethreeProduct + totalValuefourProduct + totalValuefiveProduct;
                             totalDiscount = totalpurchase-(totalpurchase*0.15);
                             printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalpurchase,37,totalDiscount);
                         }
                     }else{
-                        printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,37);
+                        printf("PRODUCTOS:\n->%s\n->%s\n->%s\n->%s\nDESCUENTO: 15%c al valor total de la compra",oneProductName,twoProductName,threeProductName,fourProductName,37);
                         totalpurchase = totalValueOneProduct + totalValueTwoProduct + totalValuethreeProduct + totalValuefourProduct;
                         totalDiscount = totalpurchase-(totalpurchase*0.15);
                         printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalpurchase,37,totalDiscount);
                     }
                 }else{
-                    printf("PRODUCTOS:\n->%s\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,threeProductName,37);
+                    printf("PRODUCTOS:\n->%s\n->%s\n->%s\nDESCUENTO: 15%c al valor total de la compra",oneProductName,twoProductName,threeProductName,37);
                     totalpurchase = totalValueOneProduct + totalValueTwoProduct + totalValuethreeProduct;
                     totalDiscount = totalpurchase-(totalpurchase*0.15);
                     printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalpurchase,37,totalDiscount);
                 }
             }else{
-                printf("PRODUCTOS:\n->%s\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,twoProductName,37);
+                printf("PRODUCTOS:\n->%s\n->%s\nDESCUENTO: 15%c al valor total de la compra",oneProductName,twoProductName,37);
                 totalpurchase = totalValueOneProduct + totalValueTwoProduct;
                 totalDiscount = totalpurchase-(totalpurchase*0.15);
                 printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalpurchase,37,totalDiscount);
             }
         }else{
-                printf("PRODUCTOS:\n->%s\nDESCUENTO: 10%c al valor total de la compra",oneProductName,37);
+                printf("PRODUCTOS:\n->%s\nDESCUENTO: 15%c al valor total de la compra",oneProductName,37);
                 totalDiscount = totalValueOneProduct-(totalValueOneProduct*0.15);
                 printf("Total compra: $%f\nTotal valor a pagar (con el 15%c de descuento): $%f",totalValueOneProduct,37,totalDiscount);
         }    
