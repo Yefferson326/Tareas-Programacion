11972280e7e45812ac3404bd61e51a82f397becf
Yefferson326 <yefferson.nunez@uptc.edu.co>
2021-09-14 17:55:04 -0500

    Se agrego la opcion de poder selecionar el ciclo con el que se desee realizar las operaciones

--- Tablas_de_multiplicar_ciclos/Tablas.c
+++ Tablas_de_multiplicar_ciclos/Tablas.c
@@ -5,26 +5,26 @@ void forClicle (int number){
     printf("Tabla del numero %d con el ciclo FOR\n",number);
     for (int i = 1; i <= 10; i++){
         result = number * i;
-        printf("-> %d * %d = %d\n",number,i,result);
+        printf("-> %d x %d = %d\n",number,i,result);
     }
 }
 void whileClicle (int number){
-    int i = 0;
+    int i = 1;
     int result;
     printf("Tabla del numero %d con el ciclo WHILE\n",number);
     while ( i <= 10){
         result = number * i;
-        printf("-> %d * %d = %d\n",number,i,result);
+        printf("-> %d x %d = %d\n",number,i,result);
         i ++;
     }
 }
 void doWhileClicle (int number){
     int result;
-    int i = 0;
+    int i = 1;
     printf("Tabla del numero %d con el ciclo DOWHILE\n",number);
     do{
         result = number * i;
-        printf("-> %d * %d = %d\n",number,i,result);
+        printf("-> %d x %d = %d\n",number,i,result);
         i ++;
     }while ( i <= 10 );
 }
@@ -32,12 +32,21 @@ void doWhileClicle (int number){
 
 int main (){
 int number;
+int numberCicle;
 printf ("TABLAS DE MULTIPLICAR\n");
 printf ("Ingrese el numero:");
 scanf ("%d",&number);
+printf("Ciclo con el que desea que se realize las operaciones:\n1.FOR\n2.WHILE\n3.DO WHILE\nSeleccione el numero del ciclo: ");
+scanf("%i",&numberCicle);
+if( numberCicle == 1){
+    forClicle(number);
+}else if (numberCicle == 2){
+    whileClicle(number);
+}else if (numberCicle == 3){
+    doWhileClicle (number);
+}else{
+    printf("Valor incorrecto. Intentelo de nuevo");
+}
 
-forClicle(number);
-whileClicle(number);
-doWhileClicle (number);
 return 0;
 }
